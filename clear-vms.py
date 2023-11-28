c9dc8431adfa9cba41583b68c3e6c484fc5736c2
Mike <mike@oconnicich.net>
2023-11-28 16:19:14 -0600

    Migrate VMs and output status

--- clear-vms.py
+++ clear-vms.py
@@ -7,6 +7,13 @@ from time import sleep
 from proxmoxer import ProxmoxAPI
 from getpass import getpass
 
+def await_task(proxmox_api, taskid, node):
+    data = {"status": ""}
+    while (data['status'] != "stopped"):
+        sleep(1.0) # Looping with no governor causes RemoteDisconnected() errors
+        data = proxmox_api.nodes(node).tasks(taskid).status.get()
+    return data
+
 def main():
     try:
         opts, args = getopt.getopt(sys.argv[1:], "h:t:m:", ["host=", "target=", "mode="])
@@ -64,10 +71,13 @@ def main():
         for node in nodes:
             if (node[nodeIndex] < lowNode[nodeIndex]):
                 lowNode = node
-        print("{0}-{1} to host {2}".format(vm['vmid'], config['name'], lowNode['node']))
-        #Execute migration
-        #Print success or error
-
+        print("{0}-{1} to host {2}...".format(vm['vmid'], config['name'], lowNode['node']), end='')
+        taskid = prox.nodes(target).qemu(vm['vmid']).migrate.post(target=lowNode['node'], online=1)
+        result = await_task(prox, taskid, target)
+        if (result['exitstatus'] == "OK"):
+            print("success")
+        else:
+            print("failed")
 
 if __name__ == "__main__":
     main()
